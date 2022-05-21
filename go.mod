ff49084dcd187053a30c67dd8e160d91069eef95
Anton K <plan.kapkan@outlook.com>
2022-05-21 16:59:24 +0300

    #3

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/plankapkan/tg-news-bot
 
 go 1.18
 
-require (
-	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-)
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+
+require github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -2,33 +2,44 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	token, _ := os.LookupEnv("BOT_API_TOKEN")
+	bot, err := tgbotapi.NewBotAPI(token)
+
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-
 	updates := bot.GetUpdatesChan(u)
-
 	for update := range updates {
 		if update.Message != nil { // If we got a message
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			msg := update.Message.Text
+			switch msg {
+			case "/start":
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome")
+				bot.Send(msg)
+			default:
+				msg := tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID)
+				bot.Send(msg)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
+			}
 		}
 	}
 }
